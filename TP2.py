3876ea1733335fa2565d8d4e70e8f374ee9a9a01
elvigourou <elvigourou@turing.local.isima.fr>
2020-01-29 14:01:50 +0100

    Cloning and setup

---
+++ TP2.py
@@ -0,0 +1,21 @@
+import halfedge_mesh
+
+# .off are supported, they shouldn't include comments
+mesh = halfedge_mesh.HalfedgeMesh("cube.off")
+
+# Returns a list of Vertex type (in order of file)--similarly for halfedges,
+# and facets
+mesh.vertices
+
+# The number of facets in the mesh
+len(mesh.facets)
+
+# Get the 10th halfedge
+mesh.halfedges[10]
+
+# Get the halfedge that starts at vertex 25 and ends at vertex 50
+#mesh.get_halfedge(25, 50)
+
+# Iterate over the vertices of the mesh
+for i in mesh.vertices:
+    print(i.get_vertex())
